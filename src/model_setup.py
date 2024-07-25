960d0127295e2db366103179c66b2a284cf89a3b
Aviral Walia <67686640+aviralwalia08@users.noreply.github.com>
2024-07-25 02:54:56 -0400

    Update model_setup.py

--- src/model_setup.py
+++ src/model_setup.py
@@ -1,9 +1,9 @@
 from langchain_openai import AzureChatOpenAI
 
 llm_chatopenai = AzureChatOpenAI(
-    api_key = 'ab40db4c57884de3bf7d8517677ab90e',
+    api_key = 'put your key here',
     api_version = '2023-03-15-preview',
-    azure_endpoint = 'https://lucidmvpopenai.openai.azure.com/',
+    azure_endpoint = 'put yours here',
     temperature = 0,
-    deployment_name = 'lucidpg'
+    deployment_name = 'model'
     )
